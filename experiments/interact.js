fe0181cd3f38fc44f5bf406cb543e4648990406a
Linn Benjamin <beli21lh@student.ju.se>
2023-09-21 11:09:13 +0200

    added interactive art

--- experiments/interact.js
+++ experiments/interact.js
@@ -1,25 +1,52 @@
+let dots = [];
+let numDots = 2000;
+
 function setup() {
-    createCanvas(1000, 1000);
-    background(255);
-  }
+  createCanvas(800, 800);
+  noStroke();
   
-  function draw() {
-    // Empty draw function for now
+  // Initialize the dots with random positions and velocities
+  for (let i = 0; i < numDots; i++) {
+    let dot = {
+      x: random(width),
+      y: random(height),
+      vx: random(-2, 2),
+      vy: random(-2, 2)
+    };
+    dots.push(dot);
   }
+}
+
+function draw() {
+  background(220);
   
-  function mouseDragged() {
-    // Draw colorful shapes shapes when the mouse is dragged
-    let randomColor = color(random(255), random(255), random(255));
-    fill(randomColor);
-    noStroke();
-    // Create random circle size
-    ellipse(mouseX, mouseY, random(20, 50));
-  }
-  
-  function keyPressed() {
-    // Clear the canvas when the "C" key is pressed
-    if (key === 'c' || key === 'C') {
-      background(220);
+  for (let i = 0; i < numDots; i++) {
+    let dot = dots[i];
+    
+    // Calculate the distance between the dot and the mouse
+    let dx = mouseX - dot.x;
+    let dy = mouseY - dot.y;
+    let distance = sqrt(dx * dx + dy * dy);
+    
+    // If the dot is too close to the mouse, avoid it
+    if (distance <100) {
+      let angle = atan2(dy, dx);
+      dot.vx -= cos(angle) * 0.2;
+      dot.vy -= sin(angle) * 0.2;
     }
+    
+    // Update the dot's position
+    dot.x += dot.vx;
+    dot.y += dot.vy;
+    
+    // Draw the dot
+    fill(255, 0, 0); // Red color
+    ellipse(dot.x, dot.y, 10, 10);
+    
+    // Wrap the dots around the canvas
+    if (dot.x < 0) dot.x = width;
+    if (dot.x > width) dot.x = 0;
+    if (dot.y < 0) dot.y = height;
+    if (dot.y > height) dot.y = 0;
   }
-  
+}
